1cb511b385531e458aa46d2a691de78cd66e333f
myhaoboyi <bh76@njit.edu>
2016-02-19 11:20:07 -0500

    PS Chrome only

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	
+	if(!!window.chrome && !!window.chrome.webstore===false){
+		console.log('here')
+   	$('body').css('display','none');
+	}
   var wechat='http://farm2.staticflickr.com/1481/24761244169_29f9f4019b_b.jpg';
   $(".2015").click(function (e) {
      e.preventDefault();
@@ -20,20 +25,6 @@ $(document).ready(function() {
     speed           : 0.6,     // popup speed (in seconds)
     showClose       : true,    // close button indicator
     })
-  	//options: {
-    //title           : 'WECHAT 扫一扫',      // title html of the popup
-    //modal           : false,   // indicates if modal
-    //width           : 300,     // width in px
-    //height          : 500,     // height in px
-    //url             : 'image/wechat.jpg',    // url to load content from (see load method)
-    //style           : '',      // additional styles
-    //color           : '#000',  // background-color for the screen lock div
-    //opacity         : 0.4,     // opacity for the screen lock div
-    //speed           : 0.6,     // popup speed (in seconds)
-    //showClose       : true,    // close button indicator
-    //
-		//};
-
-  });
+ });
 
 });
