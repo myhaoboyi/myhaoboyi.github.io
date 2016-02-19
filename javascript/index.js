8e9923ccc5d10a14d84fcf1ff70c21224185f462
myhaoboyi <bh76@njit.edu>
2016-02-19 00:25:40 -0500

    version 0.05
    version 0.05

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".2015").click(function (e) {
+     e.preventDefault();
+  	window.open("http://www.cnn.com", "_self");
+  });
+  $(".titleImage").click(function (e) {
+     e.preventDefault();
+  	window.open("http://lovemecynthia.com", "_self");
+  });
+
+});
